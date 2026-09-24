e193509b9e3396889a1a3b626e8d3b2841b1b9ef
jessicaqueirozz <jessica.santossqz@gmail.com>
2026-09-24 20:31:46 +0000

    cria estrutura inicial do projeto

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const produto = {
-    nome: "Mouse Gamer",
-    preco: 150,
-    categoria: "Periféricos"
-};
+const produtos = [
+    {
+        nome: "Mouse Gamer",
+        preco: 150,
+        categoria: "Periféricos"
+    },
+    {
+        nome: "Notebook",
+        preco: 2500,
+        categoria: "Eletrônicos"
+    },
+    {
+        nome: "Teclado Mecânico",
+        preco: 350,
+        categoria: "Periféricos"
+    },
+    {
+        nome: "Fone Bluetooth",
+        preco: 200,
+        categoria: "Áudio"
+    },
+    {
+        nome: "Monitor",
+        preco: 1200,
+        categoria: "Eletrônicos"
+    },
+    {
+        nome: "Smartphone",
+        preco: 1800,
+        categoria: "Celulares"
+    }
+];
 
 app.get("/", (req, res) => {
-    res.json(produto);
+    res.json(produtos);
 });
 
 app.listen(3000, () => {
