2fcd4785982dc184eaef904aca8ed16a72d838cb
jessicaqueirozz <jessica.santossqz@gmail.com>
2026-09-24 20:34:44 +0000

    cria API de produtos

--- frontend/script.js
+++ frontend/script.js
@@ -3,17 +3,21 @@ async function carregarDados() {
 
     const resposta = await fetch(url);
 
-    const produto = await resposta.json();
+    const produtos = await resposta.json();
 
     const listaProdutos = document.getElementById("lista-produtos");
 
-    listaProdutos.innerHTML = `
-        <div class="card">
-            <h2>${produto.nome}</h2>
-            <p>Categoria: ${produto.categoria}</p>
-            <p class="preco">Preço: R$ ${produto.preco}</p>
-        </div>
-    `;
+    listaProdutos.innerHTML = "";
+
+    produtos.forEach((produto) => {
+        listaProdutos.innerHTML += `
+            <div class="card">
+                <h2>${produto.nome}</h2>
+                <p>Categoria: ${produto.categoria}</p>
+                <p class="preco">Preço: R$ ${produto.preco}</p>
+            </div>
+        `;
+    });
 }
 
 carregarDados();
